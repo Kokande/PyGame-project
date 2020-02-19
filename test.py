9545ab88362fb175cb195af60db2f5de5b9ceabb
Kokande <sdobrovna@gmail.com>
2020-02-19 16:49:06 +0500

    Some additions to classes and program in common

--- test.py
+++ test.py
@@ -3,20 +3,27 @@ import pygame
 
 class Button:
     def __init__(self, scr, xy, wh, text="", font=(None, 100), fc=(255, 255, 255),
-                 border=(255, 255, 255), color=(0, 0, 0), bw=5):
+                 border=(255, 255, 255), color=(0, 0, 0), alt=(255, 255, 255), bw=5):
         self.coords = xy
         self.width = wh[0]
         self.height = wh[1]
-        self.color =color
+        self.color = color
         self.border = border
         self.border_weight = bw
+        self.alt = alt
         self.screen = scr
+        self.func = lambda: None
         self.text = pygame.font.Font(font[0], font[1]).render(text, 80, fc)
         self.border_color = border
+        self.calm = True
 
     def render(self):
-        pygame.draw.rect(self.screen, self.color,
-                         (self.coords[0], self.coords[1], self.width, self.height))
+        if self.calm:
+            pygame.draw.rect(self.screen, self.color,
+                             (self.coords[0], self.coords[1], self.width, self.height))
+        else:
+            pygame.draw.rect(self.screen, self.alt,
+                             (self.coords[0], self.coords[1], self.width, self.height))
         self.screen.blit(self.text, (self.coords[0] +
                                      (self.width / 2 - self.text.get_width()/2),
                                      self.coords[1] +
@@ -25,8 +32,22 @@ class Button:
                          (self.coords[0], self.coords[1], self.width, self.height),
                          self.border_weight)
 
-    def mouse_track(self, mouse):
-        pass
+    def mouse_down_track(self, mouse):
+        if self.is_overlapping(mouse.pos):
+            self.calm = False
+
+    def set_act(self, func):
+        self.func = func
+
+    def init_click(self):
+        if not self.calm:
+            self.func()
+
+    def is_overlapping(self, coords):
+        if self.coords[0] <= coords[0] <= self.coords[0] + self.width and \
+           self.coords[1] <= coords[1] <= self.coords[1] + self.height:
+            return True
+        return False
 
 
 class Game:
@@ -54,6 +75,10 @@ class Game:
 
     def change_screen(self, scr):
         self.screen = scr
+        if self.screen == 0:
+            self.set_menu()
+        elif self.screen == 1:
+            self.set_game()
 
     def render_game(self):
         pass
@@ -86,9 +111,15 @@ class Game:
     def mouse_pressed_ingame(self, mouse):
         pass
 
+    def mouse_moved(self, movement):
+        if self.screen == 0:
+            pass
+
     def set_menu(self):
         self.objects.clear()
-        self.objects['buttons'] = [Button(self.display, (50, 200), (400, 100), text="Start")]
+        self.objects['buttons'] = [Button(self.display, (50, 200), (400, 100),
+                                          text="Start", alt=(50, 50, 50))]
+        self.objects['buttons'][0].set_act(self.change_screen(1))
 
     def set_game(self):
         pass
@@ -110,6 +141,8 @@ if __name__ == '__main__':
                     game.key_pressed(event)
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 game.mouse_pressed(event)
+            elif event.type == pygame.MOUSEMOTION:
+                game.mouse_moved(event)
             elif event.type == pygame.QUIT:
                 game.kill_the_game()
         screen.fill((0, 0, 0))
