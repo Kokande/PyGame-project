e39a2f6bcb152aa073b85ddcc099c5348b82b6a1
Kokande <sdobrovna@gmail.com>
2020-03-03 01:21:47 +0500

    Full game, but with saves bug

--- test.py
+++ test.py
@@ -1,13 +1,110 @@
 import pygame
 import random
 """
-Класс кнопки
+Класс Сущности
 """
 
 
-class Enemy:
-    def __init__(self):
+class Entity:
+    def __init__(self, pos, parent, standing_on='0', type_name=None):
+        self.pos = pos
+        self.parent = parent
+        self.type_name = type_name
+        self.pause = 0
+        self.standing_on = standing_on
+        self.visibles = dict()
+
+    def move_logic(self):
+        if self.type_name == 'Enemy':
+            visible = self.get_vision()
+            self.get_visible_objects(visible)
+            if self.visibles['player'] is None or\
+                    ((self.standing_on == '.' or self.standing_on == '0') and
+                     self.standing_on != self.parent.standing_on):
+                return random.sample(['UP', 'DOWN', 'LEFT', 'RIGHT'], 4)
+            else:
+                """
+                Простой алгоритм преследования игрока:)
+                """
+                if self.visibles['player'][0] < 0 and self.visibles['player'][1] > 0:
+                    return ['DOWN', 'LEFT', 'RIGHT', 'UP']
+                elif self.visibles['player'][0] > 0 and self.visibles['player'][1] < 0:
+                    return ['UP', 'RIGHT', 'LEFT', 'DOWN']
+                elif self.visibles['player'][0] > 0 and self.visibles['player'][1] > 0:
+                    return ['DOWN', 'RIGHT', 'LEFT', 'UP']
+                elif self.visibles['player'][0] < 0 and self.visibles['player'][1] < 0:
+                    return ['UP', 'LEFT', 'RIGHT', 'DOWN']
+                elif self.visibles['player'][0] == 0 and self.visibles['player'][1] > 0:
+                    return ['DOWN', 'LEFT', 'RIGHT', 'UP']
+                elif self.visibles['player'][0] == 0 and self.visibles['player'][1] < 0:
+                    return ['UP', 'LEFT', 'RIGHT', 'DOWN']
+                elif self.visibles['player'][0] < 0 and self.visibles['player'][1] == 0:
+                    return ['LEFT', 'UP', 'DOWN', 'RIGHT']
+                elif self.visibles['player'][0] > 0 and self.visibles['player'][1] == 0:
+                    return ['RIGHT', 'UP', 'DOWN', 'LEFT']
+
+    def move(self):
+        change = self.parent.game_map
+        decision = self.move_logic()
+        moved = False
+        new_coords = self.pos
+        directions = {'UP': (self.pos[0], self.pos[1] - 1),
+                      'DOWN': (self.pos[0], self.pos[1] + 1),
+                      'LEFT': (self.pos[0] - 1, self.pos[1]),
+                      'RIGHT': (self.pos[0] + 1, self.pos[1])}
+        if decision is not None:
+            for i in decision:
+                new_coords = directions[i]
+                if self.move_possible(new_coords):
+                    change[self.pos[1]][self.pos[0]] = self.standing_on
+                    self.pos = new_coords
+                    moved = True
+                    break
+        if moved:
+            self.standing_on = change[self.pos[1]][self.pos[0]]
+            change[self.pos[1]][self.pos[0]] = 'B' if self.type_name == 'Goal' else 'E'
+        return change
+
+    def move_possible(self, new_coords):
+        if 0 <= new_coords[0] < 10 * self.parent.map_size and \
+                0 <= new_coords[1] < 10 * self.parent.map_size and \
+                self.parent.game_map[new_coords[1]][new_coords[0]] in ['0', '1', '.'] and \
+                (self.parent.game_map[new_coords[1]][new_coords[0]] == '1' or
+                 self.standing_on == self.parent.game_map[new_coords[1]][new_coords[0]] or
+                 self.standing_on == '1'):
+            return True
+        return False
+
+    def get_vision(self):
+        radius = 6
+        return [[self.parent.game_map[i][k] if 0 <= k < self.parent.map_size * 10 else '*'
+                for k in range(self.pos[0] - radius, self.pos[0] + radius + 1)]
+                if 0 <= i < self.parent.map_size * 10 else list('*' * radius * 2 + '*')
+                for i in range(self.pos[1] - radius, self.pos[1] + radius + 1)]
+
+    def get_visible_objects(self, vision):
+        self.visibles = {'player': None, 'goals': []}
+        for i in range(13):
+            for k in range(13):
+                if vision[k][i] not in ['.', '0', '1', '*']:
+                    if vision[k][i] == 'P':
+                        self.visibles['player'] = (i - 6, k - 6)
+
+    def on_iter(self):
+        if self.pause == 0:
+            self.pause = 5
+            return self.move()
+        else:
+            self.pause -= 1
+            return self.parent.game_map
+
+    def interaction(self):
         pass
+
+
+"""
+Класс кнопки
+"""
 
 
 class Button:
@@ -68,7 +165,7 @@ class Button:
 
 
 """
-Сама игра
+Класс приложения игры
 """
 
 
@@ -84,13 +181,17 @@ class Game:
     """
     class Game:
         def __init__(self, parent, map_size=3, current_lvl=1,
-                     player_pos=(-1, -1), standing_on='0', game_map=None, new=True):
+                     player_pos=(-1, -1), standing_on='0', game_map=None,
+                     new=True, entities=None, chest=(0, 0)):
+            self.entities = {'enemies': []}
             self.parent = parent
             self.map_size = map_size
             self.current_lvl = current_lvl
+            self.chest = chest
             self.map_draft = {0: [['.' for i in range(10)] for i in range(10)]}
             if game_map is None:
-                self.game_map = [[0 for k in range(self.map_size)] for i in range(self.map_size)]
+                self.game_map = [[0 for k in range(self.map_size)]
+                                 for i in range(self.map_size)]
             else:
                 self.game_map = game_map
             self.player_pos = player_pos
@@ -98,8 +199,13 @@ class Game:
             self.extract_draftables()
             if new:
                 self.map_generator()
+            if entities is None:
+                self.find_objects()
+            else:
+                self.entities['enemies'].extend(entities)
 
         def render(self):
+            self.entities_move()
             step = 40
             visible = [[self.game_map[i][k]
                         if 0 <= k < self.map_size * 10
@@ -140,6 +246,14 @@ class Game:
                     elif tile == 'C':
                         pygame.draw.rect(self.parent.display, pygame.Color('yellow'),
                                          (x, y, step, step))
+                    elif tile == 'B':
+                        pygame.draw.rect(self.parent.display, pygame.Color('cyan'),
+                                         (x, y, step, step), 4)
+
+        def entities_move(self):
+            for i in self.entities:
+                for k in self.entities[i]:
+                    self.game_map = k.on_iter()
 
         def mouse_pressed(self, mouse):
             pass
@@ -188,6 +302,8 @@ class Game:
                 self.standing_on == self.game_map[new_coords[1]][new_coords[0]] or
                self.standing_on == '1'):
                 return True
+            elif new_coords == self.chest:
+                self.victory()
             return False
 
         def extract_draftables(self):
@@ -208,9 +324,9 @@ class Game:
                 for k in range(self.map_size):
                     if i * self.map_size + k + 1 in coord_seq:
                         self.game_map[i][k] = generator[i * self.map_size + k + 1]
-            self.map_constructor()
+            self.game_constructor()
 
-        def map_constructor(self):
+        def game_constructor(self):
             map_rows = [[] for i in range(10 * self.map_size)]
             for i in range(self.map_size):
                 for k in range(self.map_size):
@@ -219,6 +335,34 @@ class Game:
                         if self.game_map[i][k] == 1 and j == 4:
                             self.player_pos = (k * 10 + 4, i * 10 + j)
             self.game_map = map_rows
+            broke = False
+            en = 10
+            for i in random.sample(range(self.map_size * 10), self.map_size * 10):
+                if broke:
+                    break
+                if '0' in self.game_map[i]:
+                    for k in random.sample(range(self.map_size * 10), self.map_size * 10):
+                        if self.game_map[i][k] == '0' and not broke:
+                            self.game_map[i][k] = 'C'
+                            self.chest = (k, i)
+                            broke = True
+                        elif self.game_map[i][k] == '.' and en > 0:
+                            en -= 1
+                            self.game_map[i][k] = 'E'
+                            self.entities['enemies'].append(Entity((k, i), self,
+                                                                   type_name='Enemy',
+                                                                   standing_on='.'))
+
+        def find_objects(self):
+            for i in range(self.map_size * 10):
+                for k in range(self.map_size * 10):
+                    if self.game_map[k][i] not in ['.', '0', '1', 'P']:
+                        if self.game_map[k][i] == 'E':
+                            self.entities['enemies'].append(Entity((i, k), self,
+                                                                   type_name='Enemy'))
+
+        def victory(self):
+            self.parent.change_screen(self.parent.Victory(self.parent))
     """
     Класс меню игры
     """
@@ -242,7 +386,7 @@ class Game:
 
         def render(self):
             self.parent.display.blit(pygame.font.Font(None,
-                                                      150).render("Project Name", 80,
+                                                      150).render("Annoying Town", 80,
                                                                   (100, 0, 0)), (50, 50))
             for i in self.objects:
                 for k in self.objects[i]:
@@ -265,6 +409,28 @@ class Game:
                     k.mouse_down_track(mouse)
 
     """
+    Победное окно
+    """
+
+    class Victory:
+        def __init__(self, parent):
+            self.parent = parent
+
+        def render(self):
+            self.parent.display.blit(pygame.font.Font(None,
+                                                      150).render("Победа!", 80,
+                                                                  (100, 0, 0)), (50, 50))
+
+        def mouse_move(self, mouse):
+            pass
+
+        def key_pressed(self, key):
+            pass
+
+        def mouse_pressed(self, mouse):
+            pass
+
+    """
     Методы класса Game
     """
     def load_game(self):
@@ -274,10 +440,16 @@ class Game:
         current_lvl = int(file.pop(0).rstrip())
         player_pos = tuple([int(i) for i in file.pop(0).rstrip()[1:-1].split(', ')])
         standing_on = file.pop(0).rstrip()
+        enemies = []
+        e_d = file.pop(0).rstrip().split()
+        for i in range(0, len(e_d), 3):
+            enemies.append(Entity(tuple([int(k)
+                                         for k in [e_d[i][1:-1], e_d[i + 1][:-1]]]),
+                                  self.game, standing_on=e_d[i + 2]))
         game_map = [i.rstrip().split() for i in file]
         self.game = self.Game(self, map_size=map_size, current_lvl=current_lvl,
                               player_pos=player_pos, standing_on=standing_on,
-                              game_map=game_map, new=False)
+                              game_map=game_map, new=False, entities=enemies)
         if type(self.screen) == Game.Menu:
             self.screen.objects['buttons'][0].set_act(self.change_screen,
                                                       value=self.game)
@@ -288,6 +460,9 @@ class Game:
                        str(self.game.current_lvl) + '\n' +
                        str(self.game.player_pos) + '\n' +
                        self.game.standing_on + '\n')
+            for i in self.game.entities['enemies']:
+                save.write(str(i.pos) + ' ' + i.standing_on + ' ')
+            save.write('\n')
             for i in self.game.game_map:
                 save.write(' '.join(i) + '\n')
 
@@ -310,9 +485,6 @@ class Game:
         else:
             self.mouse_down = False
             self.screen.mouse_pressed(mouse)
-
-    def get_screen_obj_info(self):
-        pass
 
     def mouse_moved(self, mouse):
         self.screen.mouse_moved(mouse)
